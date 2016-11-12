207cbc7e99d4b155057718650464c24df6f4a2ae
maxvanbel <Max@Maximes-MacBook-Pro.local>
2016-11-12 11:01:10 -0600

    add firebase push

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 // Initialize Firebase
-
 var config = {
-  apiKey: "AIzaSyCntfMzucYsODh3wgxLryNZ_zArQAG1WUM",
-  authDomain: "employeedata-36e36.firebaseapp.com",
-  databaseURL: "https://employeedata-36e36.firebaseio.com",
-  storageBucket: "employeedata-36e36.appspot.com",
-  messagingSenderId: "406481332206"
+    apiKey: "AIzaSyCntfMzucYsODh3wgxLryNZ_zArQAG1WUM",
+    authDomain: "employeedata-36e36.firebaseapp.com",
+    databaseURL: "https://employeedata-36e36.firebaseio.com",
+    storageBucket: "employeedata-36e36.appspot.com",
+    messagingSenderId: "406481332206"
 };
 firebase.initializeApp(config);
-
 // get reference to database
 var database = firebase.database();
-
 // initialize variables
 var name = "";
 var role = "";
 var startDate = "";
 var monthlyRate = 0;
-
 // read data from firebase if it exists
-
-
 // get values from inputs
-
-
 // push data to firebase
+$('#submit').on('click', function () {
+	name = $('#inputName').val().trim();
+	role = $('#inputRole').val().trim();
+	startDate = $('#inputDate').val().trim();
+	monthlyRate = $('#inputMonth').val().trim();
 
-
+    database.ref().push({
+        name: name,
+        role: role,
+        startDate: startDate,
+        monthlyRate: monthlyRate
+    });
+});
 // clear data from form fields
-
-
 // update DOM with user data
