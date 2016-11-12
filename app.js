ba7d8be61b6ea182d9bb0af30c2d019a193733a3
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-11-12 10:32:13 -0600

    Add app.js and firebase

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+
+var config = {
+  apiKey: "AIzaSyCntfMzucYsODh3wgxLryNZ_zArQAG1WUM",
+  authDomain: "employeedata-36e36.firebaseapp.com",
+  databaseURL: "https://employeedata-36e36.firebaseio.com",
+  storageBucket: "employeedata-36e36.appspot.com",
+  messagingSenderId: "406481332206"
+};
+firebase.initializeApp(config);
